8bf6006feff75f98c37224015308c29afeb9b069
mati <matibesme@gmail.com>
2023-10-31 11:08:21 -0300

    upd

--- acciones/acciones.go
+++ acciones/acciones.go
@@ -16,86 +16,83 @@ func AccionIngresarVotante(dni_string string, cola cola.Cola[int], padron []voto
 
 	if err != nil || dni <= 0 {
 		return errores.DNIError{}
-	} else {
-		votante := EstaEnPadron(dni, padron)
-		if votante == -1 {
-			return errores.DNIFueraPadron{}
-		} else {
-			cola.Encolar(votante)
-			return nil
-		}
 	}
+	votante := EstaEnPadron(dni, padron)
+	if votante == -1 {
+		return errores.DNIFueraPadron{}
+	}
+	cola.Encolar(votante)
+	return nil
+
 }
 
 func AccionVotar(entrada []string, cola cola.Cola[int], padron []votos.Votante, crear_partidos []votos.Partido, lista_partidos []string) error {
 
 	if len(entrada) != 3 {
 		return errores.ErrorParametros{}
-
-	} else {
-
-		cargo, err2 := verificoCargoAVotar(entrada[1])
-		candidato, err := strconv.Atoi(entrada[2])
-
-		if cola.EstaVacia() {
-			return errores.FilaVacia{}
-
-		} else if err2 != nil || cargo == 3 {
-			return errores.ErrorTipoVoto{}
-		} else if err != nil || !partidoValido(lista_partidos, candidato) {
-			return errores.ErrorAlternativaInvalida{}
-		} else {
-			votar := (padron[cola.VerPrimero()]).Votar(cargo, candidato)
-
-			if votar == nil {
-				return nil
-			} else {
-				//error de votar
-				cola.Desencolar()
-				return votar
-			}
-
-		}
 	}
+
+	cargo, err2 := verificoCargoAVotar(entrada[1])
+	candidato, err := strconv.Atoi(entrada[2])
+
+	if cola.EstaVacia() {
+		return errores.FilaVacia{}
+	}
+	if err2 != nil || cargo == 3 {
+		return errores.ErrorTipoVoto{}
+	}
+	if err != nil || !partidoValido(lista_partidos, candidato) {
+		return errores.ErrorAlternativaInvalida{}
+	}
+	votar := (padron[cola.VerPrimero()]).Votar(cargo, candidato)
+
+	if votar == nil {
+		return nil
+	}
+
+	cola.Desencolar()
+	return votar
+
 }
 
 func AccionDeshacer(cola cola.Cola[int], padrones []votos.Votante) error {
 
 	if cola.EstaVacia() {
 		return errores.FilaVacia{}
-	} else {
-		deshacer := padrones[cola.VerPrimero()].Deshacer()
-		if deshacer == (errores.ErrorNoHayVotosAnteriores{}) {
-			return deshacer
-		} else if deshacer == nil {
-			return nil
-		} else {
-			cola.Desencolar()
-			return deshacer
-		}
 	}
+	deshacer := padrones[cola.VerPrimero()].Deshacer()
+	if deshacer == (errores.ErrorNoHayVotosAnteriores{}) {
+		return deshacer
+	}
+	if deshacer == nil {
+		return nil
+	}
+	cola.Desencolar()
+	return deshacer
+
 }
 
 func AccionFinVotante(cola cola.Cola[int], padrones []votos.Votante, partidos []votos.Partido, impugnados *int) error {
 	if cola.EstaVacia() {
 		return errores.FilaVacia{}
-	} else {
-		votante_actual := padrones[cola.Desencolar()]
-		datos, err := votante_actual.FinVoto()
-		if err != nil {
-			return err
-		} else if datos.Impugnado {
-			*impugnados++
-			return nil
-		} else {
-
-			for i := 0; i < 3; i++ {
-				partidos[datos.VotoPorTipo[i]].VotadoPara(votos.TipoVoto(i))
-			}
-			return nil
-
-		}
 	}
+	votante_actual := padrones[cola.Desencolar()]
+	datos, err := votante_actual.FinVoto()
+
+	if err != nil {
+		return err
+	}
+
+	if datos.Impugnado {
+		*impugnados++
+		return nil
+	}
+
+	for i := 0; i < 3; i++ {
+		partidos[datos.VotoPorTipo[i]].VotadoPara(votos.TipoVoto(i))
+	}
+	return nil
+
 }
 
 func AccionResultadosElectorales(partidosCreados []votos.Partido, cola_voto cola.Cola[int], padrones []votos.Votante, impugnados *int) {
